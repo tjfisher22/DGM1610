0c0267e7136cb7402699c5ec6425cb165aa17396
TJ <tjfisher2015@gmail.com>
2019-01-14 17:17:00 -0700

    Scripting First time

---
+++ 2DGame/Assets/Scripts/InClassScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InClassScript : MonoBehaviour {
+
+    public int WaterBottle = 90;
+    
+	// Use this for initialization
+	void Start () {
+		print(WaterBottle);
+        Debug.Log(WaterBottle);
+        WaterBottle = 80;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print(WaterBottle);
+        WaterBottle = WaterBottle - 1;
+        
+        
+	}
+}
