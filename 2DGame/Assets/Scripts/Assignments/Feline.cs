61f646d9212794d67d78e0cdc8d20cc40c47dcea
TJ <tjfisher2015@gmail.com>
2019-02-11 17:18:31 -0700

    Pickup and inheritance

---
+++ 2DGame/Assets/Scripts/Assignments/Feline.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feline : Animal {
+
+	public bool tail;
+	public bool pointedEars;
+	public bool longHair;
+	public bool declawed; 
+	public bool domesticated;
+	public float weight;
+	public float height;
+	public float agility;
+	public string furColour;
+	public int age;
+	
+
+
+}
