9f296a14e9738582ab7a97b0a18e3ff49a61b364
TJ <tjfisher2015@gmail.com>
2019-03-10 19:18:05 -0600

    Health Bar

---
+++ 2DGame/Assets/Scripts/healthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthBar : MonoBehaviour {
+
+	private Transform bar;
+	
+	public Player player;
+
+
+	// Use this for initialization
+	void Start () {
+		bar = transform.Find("Bar");
+	}
+	
+	void Update(){
+		bar.localScale = new Vector2(player.health/100f,1f);
+	}
+}
