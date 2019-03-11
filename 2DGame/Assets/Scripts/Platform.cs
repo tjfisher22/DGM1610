5822f755ee5a450e1641c85187a35462cc2ef999
TJ <tjfisher2015@gmail.com>
2019-03-11 17:08:44 -0600

    Platform Movement Code

---
+++ 2DGame/Assets/Scripts/Platform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Platform", menuName = "Platform")]
+
+public class Platform : ScriptableObject {
+
+	public float xSpeed;
+	public float ySpeed;
+	public float xDist;
+	public float yDist;
+
+	public string name;
+
+	public Sprite charSprite;
+
+	//add stuff about prefabs here? 
+	//also add box colliders to this if possible.
+
+	
+}
---
+++ 2DGame/Assets/Scripts/PlatformControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformControl : MonoBehaviour {
+
+	public Platform plat;
+	bool left;
+	bool up;
+
+	// Use this for initialization
+	void Start () {
+		up = left = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movePlat();
+	}
+	void movePlat(){
+		//if()
+		GetComponent<Rigidbody2D>().velocity = new Vector2(plat.xSpeed,plat.ySpeed);
+	}
+}
