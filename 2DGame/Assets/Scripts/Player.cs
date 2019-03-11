e675922d95158a1af12a05f33d3877d540d71d7e
TJ <tjfisher2015@gmail.com>
2019-03-10 19:17:40 -0600

    Player ScriptableObject and Data

---
+++ 2DGame/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Player", menuName = "Player")]
+public class Player : ScriptableObject {
+	
+	public int health;
+	public int strength;
+	public int jumpHeight;
+	
+	public float speed;
+
+	public Sprite charSprite;
+
+	public new string name;
+
+
+}
