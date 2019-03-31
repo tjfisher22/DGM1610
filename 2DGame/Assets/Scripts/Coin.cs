bf98f6d2986191bdcabbbef5552f0a600972a950
TJ <tjfisher2015@gmail.com>
2019-03-31 17:44:00 -0600

    Coin Collectable

---
+++ 2DGame/Assets/Scripts/Coin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Collectables/Coin")]
+public class Coin : Collectable {
+
+	public Color coinColor = Color.black;
+	//public Sprite cSprite;
+
+	private SpriteRenderer collectSprite;
+
+	//private CollectableControl coinSpawn;
+
+	public override void Initialize(GameObject obj){
+		//coinSpawn = obj.GetComponent<CollectableControl> ();
+		//coinSpawn.Initialize ();
+		collectSprite = obj.GetComponent<SpriteRenderer> ();
+		collectSprite.sprite = cSprite;
+		collectSprite.color = coinColor;
+		collectSprite.sortingOrder = 100;
+
+
+
+	}
+	public override void Spawn(){
+
+		//coinSpawn.Spawn;
+	}
+}
